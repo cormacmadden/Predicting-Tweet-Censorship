5415f60f71cd958321111f78d16c6fbf5077a0aa
cormacmadden <comadden@Gmail.com>
2022-11-23 22:16:11 +0000

    fixed importing & added search for censored tweets

--- Src/Censorship_Prediction.py
+++ Src/Censorship_Prediction.py
@@ -8,19 +8,16 @@ import gzip
 fileDir = os.path.dirname(__file__)
 mypath = os.path.join(fileDir, '../Data/20210823')
 onlyfiles = [ f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath,f)) ]
-files = np.empty(len(onlyfiles), dtype=object)
-#for n in range(0, len(onlyfiles)):
-#    with gzip.open(os.path.join(mypath,onlyfiles[n]), mode="rt") as f:
-#        files[n] = f.read()
-for n in range(0, 3):
-    with gzip.open(os.path.join(mypath,onlyfiles[n]), mode="rt") as f:
-        files[n] = f.read()
+tweets = np.empty(len(onlyfiles), dtype=object)
+for n in range(0, len(onlyfiles)):
+    with gzip.open(os.path.join(mypath,onlyfiles[n]), mode="rb") as f:
+        tweets = [json.loads(line) for line in f]
+    if "withheld_in_countries" in tweets[n]:
+        print("test")
+        if tweets[n]['withheld_in_countries'] != [] :
+            print(tweets[n]['text'])
 
-print(files[0])
 
-text_file = open("test.json", "w")
-n = text_file.write(json.dumps(files[0]))
-text_file.close()
 
 '''
 tf.keras.preprocessing.text.text_to_word_sequence(
