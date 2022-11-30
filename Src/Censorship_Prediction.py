627cf6a9101599bff1f4dfe192a0ad0c885dd277
cormacmadden <comadden@Gmail.com>
2022-11-30 21:12:12 +0000

    clean up, and printing tweets

--- Src/Censorship_Prediction.py
+++ Src/Censorship_Prediction.py
@@ -8,26 +8,10 @@ import googleSentimentAnalysis
 import pandas as pd
 
 fileDir = os.path.dirname(__file__)
-mypath = os.path.join(fileDir, '../Data/20210823')
-onlyfiles = [ f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath,f)) ]
-tweets = np.empty(len(onlyfiles), dtype=object)
-#for n in range(0, len(onlyfiles)):
-with gzip.open(os.path.join(mypath,onlyfiles[0]), mode="rb") as f:
-    tweets = [json.loads(line) for line in f]
-    
-for n in range(0, len(tweets)):
-    if "created_at" in tweets[n]:    
-        if "withheld_in_countries" in tweets[n]:
-            print("test")
-            if tweets[n]['withheld_in_countries'] != [] :
-                print(tweets[n]['text'])
 
-
-
-
-censored_tweets = pd.read_csv('tweet_text.csv')
+censored_tweets = pd.read_csv(os.path.join(fileDir, 'tweet_text.csv'))
 print(censored_tweets.head()) # print first 5 rows
 
-for tweet in censored_tweets:
-    tweet_entities_sentiment = googleSentimentAnalysis.sample_analyze_entity_sentiment(tweet)
+for tweet in censored_tweets.text:
+    print(tweet)
     #Add to censored tweets dataframe
